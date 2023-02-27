dab6708339723cda16d76d29818ee2f641a70e9f
nvbinh15 <binh.pdc01@gmail.com>
2023-02-27 22:26:19 +0100

    Roll back original config

--- site.config.js
+++ site.config.js
@@ -1,27 +1,31 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Binh",
-    image: "/binh.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "",
-    bio: "",
-    email: "nguyen.binh@u.nus.edu",
-    linkedin: "nvbinh",
-    github: "nvbinh15",
+    name: "morethanmin",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "frontend developer",
+    bio: "I develop everything using node.",
+    email: "morethanmin.dev@gmail.com",
+    linkedin: "morethanmin",
+    github: "morethanmin",
     instagram: "",
   },
   projects: [
+    {
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
+    },
   ],
   // blog setting (required)
   blog: {
-    title: "nvbinh-log",
-    description: "welcome to Binh's blog!",
+    title: "morethan-log",
+    description: "welcome to morethan-log!",
     theme: "light", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://nvbinh-blog.vercel.app/",
-  since: 2023, // If leave this empty, current year will be used.
+  link: "https://morethan-log.vercel.app",
+  since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
@@ -39,19 +43,19 @@ const CONFIG = {
     config: {
       measurementId: process.env.GOOGLE_MEASUREMENT_ID || "",
     },
-  },  
-  utterances: {
-    enable: true,
-    config: {
-      repo: "nvbinh15/morethan-log",
-      "issue-term": "og:title",
-      label: "💬 Utterances",
-    },
   },
   googleSearchConsole: {
     enable: false,
     config: {
       siteVerification: process.env.GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: "morethanmin/morethan-log",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
     },
   },
   cusdis: {
