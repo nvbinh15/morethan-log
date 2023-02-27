0d1660ff8b00fa196e5b46af676547bad0833fa3
nvbinh15 <binh.pdc01@gmail.com>
2023-02-27 22:09:35 +0100

    Update config

--- site.config.js
+++ site.config.js
@@ -1,21 +1,12 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: 'nvbinh',
     image: '/binh.png',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: 'Machine Learning / Software Engineer',
-    bio: 'I develop everything using node.',
     email: 'nguyen.binh@u.nus.edu',
     linkedin: 'nvbinh',
     github: 'nvbinh15',
     instagram: '',
   },
-  projects: [
-    {
-      name: `morethan-log`,
-      href: 'https://github.com/morethanmin/morethan-log'
-    }
-  ],
   // blog setting (required)
   blog: {
     title: "Binh's Blog",
@@ -25,9 +16,8 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: 'https://morethan-log.vercel.app',
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
   },
@@ -48,14 +38,6 @@ const CONFIG = {
     enable: false,
     config: {
       siteVerification: process.env.GOOGLE_SITE_VERIFICATION || ''
-    }
-  },
-  utterances: {
-    enable: true,
-    config: {
-      repo: 'morethanmin/morethan-log',
-      'issue-term': 'og:title',
-      label: '💬 Utterances',
     }
   },
   isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
